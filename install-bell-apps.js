877e66f8a8ac027e9bece3906c431b80ff4bfe33
rjsteinert <rj@rjsteinert.com>
2014-01-06 08:54:15 -0500

    renamed files

--- install-bell-apps.js
+++ install-bell-apps.js
@@ -1,6 +1,6 @@
 if (!($ = window.jQuery)) { // typeof jQuery=='undefined' works too  
     scriptJQuery = document.createElement( 'script' );  
-   	scriptJQuery.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';   
+    scriptJQuery.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';   
     scriptJQuery.onload=loadPouchDB;  
     document.body.appendChild(scriptJQuery);  
 }   
@@ -10,36 +10,36 @@ else {
 
 function loadPouchDB() {
   scriptPouchDB = document.createElement( 'script' );  
- 	scriptPouchDB.src = 'http://download.pouchdb.com/pouchdb-nightly.min.js';   
+  scriptPouchDB.src = 'http://download.pouchdb.com/pouchdb-nightly.min.js';   
   scriptPouchDB.onload=installBellApps;  
   document.body.appendChild(scriptPouchDB);  
 }
   
 function installBellApps() {
-	var source = 'http://bellsource:install@bellsource.cloudant.com'
-	var db = new PouchDB(source + '/replicator');
-	// Get all of the Docs in the `replicator` database, they describe how each replication should be run
-	db.allDocs({include_docs: true}, function(err, response) { 
-		var replicationEntries = response.docs
-		var i = 0
-		// A recursive function
-		var replicate = function() {
-			var replicatorEntry = replicationEntries[i]
-			PouchDB.replicate(replicatorEntry.source, replicatorEntry.target, {
-				create_target: true,
-				complete: function() {
-					if (replicationEntries.length <= i) {
-						i++
-						replicate()
-					}
-					else {
-						alert("Installation has completed")
-						window.location = window.location.host + '/apps/_design/bell/lms/index.html'
-					}
-				}
-			})
-		}
-		// Start the recursion
-		replicate()
-	}); 
+  var source = 'http://bellsource:install@bellsource.cloudant.com'
+  var db = new PouchDB(source + '/replicator');
+  // Get all of the Docs in the `replicator` database, they describe how each replication should be run
+  db.allDocs({include_docs: true}, function(err, response) { 
+    var replicationEntries = response.docs
+    var i = 0
+    // A recursive function
+    var replicate = function() {
+      var replicatorEntry = replicationEntries[i]
+      PouchDB.replicate(replicatorEntry.source, replicatorEntry.target, {
+        create_target: true,
+        complete: function() {
+          if (replicationEntries.length <= i) {
+            i++
+            replicate()
+          }
+          else {
+            alert("Installation has completed")
+            window.location = window.location.host + '/apps/_design/bell/lms/index.html'
+          }
+        }
+      })
+    }
+    // Start the recursion
+    replicate()
+  }); 
 } 
