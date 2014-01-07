b3d5a8dcd53f86e058263eff79609c8c958f2c55
rjsteinert <rj@rjsteinert.com>
2014-01-06 22:22:13 -0500

    No need for a target, it's just local.

--- install-bell-apps.js
+++ install-bell-apps.js
@@ -1,13 +1,12 @@
 
 var source = 'http://bellappssource:installpass@bellappssource.cloudant.com'
-var target = window.location.host
 var i = 0
 // @todo Need the rest of the databases here
 var databases = ['apps', 'facilities', 'members', 'feedback']
 // A recursive function to replicate the databases one at a time
 var replicate = function() {
   var database = databases[i]
-  $.couch.replicate(source + '/' + database, target + '/' + database, {
+  $.couch.replicate(source + '/' + database, database, {
     create_target: true,
     success: function() {
       if (databases.length <= i) {
