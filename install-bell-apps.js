80bd6e45464c367c104f315b78548710f1670e36
rjsteinert <rj@rjsteinert.com>
2014-01-06 22:16:31 -0500

    using jquery.couch.js instead of pouch due to Access-Control-Allow-Origin rules.

--- install-bell-apps.js
+++ install-bell-apps.js
@@ -1,18 +1,18 @@
 if (!($ = window.jQuery)) { // typeof jQuery=='undefined' works too  
     scriptJQuery = document.createElement( 'script' );  
     scriptJQuery.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';   
-    scriptJQuery.onload=loadPouchDB;  
+    scriptJQuery.onload=loadCouchJS;  
     document.body.appendChild(scriptJQuery);  
 }   
 else {  
-    loadPouchDB;  
+    loadCouchJS;  
 }  
 
-function loadPouchDB() {
-  scriptPouchDB = document.createElement( 'script' );  
-  scriptPouchDB.src = 'http://download.pouchdb.com/pouchdb-nightly.min.js';   
-  scriptPouchDB.onload=installBellApps;  
-  document.body.appendChild(scriptPouchDB);  
+function loadCouchJS() {
+  scriptCouchJS = document.createElement( 'script' );  
+  scriptCouchJS.src = '/_utils/script/jquery.couch.js';   
+  scriptCouchJS.onload=installBellApps;  
+  document.body.appendChild(scriptCouchJS);  
 }
   
 function installBellApps() {
@@ -24,9 +24,9 @@ function installBellApps() {
   // A recursive function to replicate the databases one at a time
   var replicate = function() {
     var database = databases[i]
-    PouchDB.replicate(source + '/' + database, target + '/' + database, {
+    $.couch.replicate(source + '/' + database, target + '/' + database, {
       create_target: true,
-      complete: function() {
+      success: function() {
         if (databases.length <= i) {
           i++
           replicate()
@@ -35,7 +35,11 @@ function installBellApps() {
           alert("Installation has completed")
           window.location = window.location.host + '/apps/_design/bell/lms/index.html'
         }
-      }
+      },
+      error: function(status) {
+        console.log(status);
+        alert('Something went wrong:' + status)
+    	}
     })
   }
   // Start the recursion
