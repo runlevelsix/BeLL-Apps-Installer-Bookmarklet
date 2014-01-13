1b8d20821eba32daf21db464b06fdaa6ba998f32
rjsteinert <rj@rjsteinert.com>
2014-01-13 14:23:55 -0500

    Big update, prompts for Source Server now.

--- install-bell-apps.js
+++ install-bell-apps.js
@@ -1,20 +1,21 @@
-var source = 'http://bellappssource:installpass@bellappssource.cloudant.com'
+
+var source = prompt('Which source server are you installing from?', 'http://bellappssource:installpass@bellappssource.cloudant.com')
 var i = 0
 alert("Install script loaded. Now replicating data from " + source)
 // @todo Need the rest of the databases here
-var databases = ['apps', 'facilities', 'members', 'feedback']
+var databases = ["actions","apps","assignments","calendar","community","communityreports","courseschedule","coursestep","facilities","feedback","groups","install","invitations","mail","membercourseprogress","members","report","resources","shelf","stepresults","sync"]
 // A recursive function to replicate the databases one at a time
 var replicate = function() {
   var database = databases[i]
   $.couch.replicate(source + '/' + database, database, {
     success: function() {
-      if (databases.length > i) {
+      if (databases.length-1 > i) {
         i++
         replicate()
       }
       else {
         alert("Installation has completed")
-        window.location = window.location.host + '/apps/_design/bell/lms/index.html'
+        window.location = 'http://' + window.location.host + '/apps/_design/bell/lms/index.html'
       }
     },
     error: function(status) {
